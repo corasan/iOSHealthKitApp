08c8009698027d614f9e0052f7602fc8473aee76
Henry Paulino <henrypl360@gmail.com>
2018-10-18 11:23:08 -0400

    Initial Commit

---
+++ HealthKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKitApp
+//
+//  Created by Henry Paulino on 10/18/18.
+//  Copyright © 2018 Henry Paulino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
