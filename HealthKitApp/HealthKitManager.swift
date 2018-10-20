6bbfec3564c06bd5c55e9b3de61a15a11aad5a37
Henry Paulino <henrypl360@gmail.com>
2018-10-20 00:32:13 -0400

    display calories, add constraints, and clean code

--- HealthKitApp/HealthKitManager.swift
+++ HealthKitApp/HealthKitManager.swift
@@ -19,15 +19,20 @@ class HealthKitManager {
 	
 	func getSteps(callback: @escaping (Double) -> Void) {
 		let stepsCountType = HKObjectType.quantityType(forIdentifier: .stepCount)!
-		let now = Date()
-		let startOfDay = Calendar.current.startOfDay(for: now)
-		let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+		let predicate = createPredicate()
 		
-		let query = HKStatisticsQuery(quantityType: stepsCountType, quantitySamplePredicate: predicate, options: .cumulativeSum) { _, result, _ in
-			guard let result = result, let sum = result.sumQuantity() else {
-				return
-			}
-			callback(sum.doubleValue(for: HKUnit.count()))
+		let query = statisticsQuery(quantityType: stepsCountType, predicate: predicate, calcType: nil) { (result) in
+			callback(result)
+		}
+		healthStore?.execute(query)
+	}
+	
+	func getCalories(callback: @escaping (Double) -> Void) {
+		let caloriesType = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!
+		let predicate = createPredicate()
+		
+		let query = statisticsQuery(quantityType: caloriesType, predicate: predicate, calcType: "calories") { (result) in
+			callback(result)
 		}
 		healthStore?.execute(query)
 	}
@@ -53,4 +58,27 @@ class HealthKitManager {
 			}
 		}
 	}
+	
+	private func createPredicate() -> NSPredicate {
+		let now = Date()
+		let startOfDay = Calendar.current.startOfDay(for: now)
+		
+		return HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+	}
+	
+	private func statisticsQuery(quantityType: HKQuantityType, predicate: NSPredicate, calcType: String?, completion: @escaping (Double) -> Void) -> HKStatisticsQuery {
+		let query = HKStatisticsQuery(quantityType: quantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { _, result, _ in
+			guard let result = result, let sum = result.sumQuantity() else {
+				return
+			}
+			
+			if (calcType == "calories") {
+				completion(sum.doubleValue(for: HKUnit.kilocalorie()))
+			} else {
+				completion(sum.doubleValue(for: HKUnit.count()))
+			}
+		}
+		
+		return query
+	}	
 }
--- HealthKitApp/ViewController.swift
+++ HealthKitApp/ViewController.swift
@@ -10,25 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 	@IBOutlet weak var steps: UILabel!
+	@IBOutlet weak var calories: UILabel!
 	
 	var healthkitStore: HealthKitManager?
 	var stepCount: Int = 0
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		steps.sizeToFit()
+		calories.sizeToFit()
 		healthkitStore = HealthKitManager()
 		
 		setSteps()
+		setCalrories()
 	}
 
 	func setSteps() {
 		healthkitStore?.getSteps(callback: { (stepCount) in
-			print(stepCount)
 			DispatchQueue.main.async {
-				self.steps.text = String(stepCount)
+				self.steps.text = self.formatDouble(stepCount)
 			}
 		})
+	}
+	
+	func setCalrories() {
+		healthkitStore?.getCalories(callback: { (calories) in
+			DispatchQueue.main.async {
+				self.calories.text = self.formatDouble(calories)
+			}
+		})
+	}
+	
+	private func formatDouble(_ number: Double) -> String {
+		return String(format: "%.0f", number)
 	}
 }
 
